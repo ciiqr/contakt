dc06675c330cb99de7e71bdcae6499abbd764d8b
William Villeneuve <willvill1995@gmail.com>
2016-01-25 14:35:14 -0500

    Made it more like the example to remove some variables...

--- .test.sh
+++ .test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-TEST_CMD="xcodebuild -scheme contakt -project contakt/contakt.xcodeproj -sdk iphonesimulator9.2 -destination 'platform=iOS Simulator,name=iPhone 6s Plus,OS=9.2' build test"
+TEST_CMD="xcodebuild -scheme contakt -project contakt/contakt.xcodeproj -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6s,OS=9.1' build test"
 
 which -s xcpretty
 XCPRETTY_INSTALLED=$?
